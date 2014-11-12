0a0bdf4a05637d97c167db7bccd698aafe8ae871
timflew <timflew@users.noreply.github.com>
2014-11-12 15:46:39 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from __future__ import division
+
+import drawer
+import timer
+import time
+from numpy import *
+from inputter import *
+
+def showFeedback(fbtext, xstart=5, ystart=100):
+	drawer.fillScreen((0,0,0))
+	for i in range(0,len(fbtext)):
+		drawer.writeText(fbtext[i], (xstart,ystart+30*i))
+	drawer.flip()
+	waitforkey()
+
+
+def instructions(insfile):
+	insttext = ['show instructions here']
+	showFeedback(insttext)
+
+
+drawer = drawer.Drawer()  
+instructions('instructions.txt')
+
+
+quit()
